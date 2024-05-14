11457d7593565eff475b9a6a403e84e6f15e078d
Tarcisiomn96 <65929523+Tarcisiomn96@users.noreply.github.com>
2024-05-14 11:37:43 -0300

    Update CategoriaEntity.java

--- src/main/java/com/stoom/produtos/model/entity/CategoriaEntity.java
+++ src/main/java/com/stoom/produtos/model/entity/CategoriaEntity.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class CategoriaEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String codigo;
