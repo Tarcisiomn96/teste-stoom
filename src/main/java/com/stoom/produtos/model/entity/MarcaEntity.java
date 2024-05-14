250ee3a021f9fc8498dbea21288ebf29a2b66b9a
Tarcisiomn96 <65929523+Tarcisiomn96@users.noreply.github.com>
2024-05-14 11:37:55 -0300

    Update MarcaEntity.java

--- src/main/java/com/stoom/produtos/model/entity/MarcaEntity.java
+++ src/main/java/com/stoom/produtos/model/entity/MarcaEntity.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 public class MarcaEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String codigo;
